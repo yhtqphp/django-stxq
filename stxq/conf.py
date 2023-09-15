da1adb4796eef3122f372eacb427381b3d714ffb
段周渝 <you@example.com>
2023-09-15 21:20:58 +0800

    11

--- stxq/conf.py
+++ stxq/conf.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 
-
 class Conf:
     conf = settings.S_TXQ
     # pid路径
-    pid = conf.get('pid', settings.BASE_DIR +'/stxq.pid')
+    pid = conf.get('pid', settings.BASE_DIR / 'stxq.pid')
     # 检测进程时间, 单位秒
     checkProcTimeout = conf.get('check_proc_timeout', 1)
     process = conf.get('process', [])
